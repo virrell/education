8b42426c0fe5356cd19acdb8e5fa56b28883e3ab
Evgeny Surovikin <virrell.55@gmail.com>
2020-10-29 00:28:49 +0600

    Добавлены методы CRUD для PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function createPost(Request $req){
+        $post = new Post();
+        $post->post_content = $req->input('post-content');
+        $post->save();
+
+        return redirect()->route('posts');
+    }
+
+    public function deletePost($id){
+        Post::find($id)->delete();
+        return redirect()->route('posts');
+    }
+
+    public function updatePost($id, Request $req) {
+        $post = Post::find($id);
+        $post->post_content = $req->input('post-content');
+        $post->save();
+        return redirect()->route('posts');
+    }
+
+    public function getAllPosts(Request $req)
+    {
+        $posts = Post::all();
+
+        return view('home', ['posts' => $posts]);
+    }
+
+    public function getPost($id)
+    {
+        $post = Post::find($id);
+
+        return view('post', ['data' => $post]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use GuzzleHttp\Psr7\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+
+Route::get('welcome', function () {
     return view('welcome');
 });
+
+Route::get('posts', [PostController::class, 'getAllPosts'])->name('posts');
+Route::get('posts/{id}', [PostController::class, 'getPost'])->name('post');
+
+Route::get('post/create', function(){
+    return view('create-post');
+});
+
+
+Route::post('/post/submit', [PostController::class, 'createPost'])->name('create-post');
+Route::post('/posts/{id}', [PostController::class, 'updatePost'])->name('post-update');
+Route::post('/posts/{id}/delete', [PostController::class, 'deletePost'])->name('post-delete');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/', function () {
+    route('posts');
+});
